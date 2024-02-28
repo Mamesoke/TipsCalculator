f136becce66803f6a8d093c30601d332926769a6
Kevin M. <mamesoke@gmail.com>
2024-02-28 10:04:18 +0100

    Refactor UITests

--- TipCalculatorUITests/TipCalculatorUITests.swift
+++ TipCalculatorUITests/TipCalculatorUITests.swift
@@ -8,24 +8,19 @@
 import XCTest
 
 final class TipCalculatorUITests: XCTestCase {
+    
+    private var app: XCUIApplication!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = runApp()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        app = nil
     }
 
     func testGivenContentViewWhenAppInitThenIsShown() throws {
         let expected = "Enter total"
-        
-        let app = runApp()
 
         let totalTextField = app.textFields["totalTextField"]
         
@@ -34,8 +29,6 @@ final class TipCalculatorUITests: XCTestCase {
     
     func testGiven20PercentWhenDefaultTipOptionThenIsSelected() throws {
         let expected = "20%"
-        
-        let app = runApp()
         
         let tipPercentageSegmentedControl = app.segmentedControls["tipPercentageSegmentedControl"]
         let segmentedControlButton = tipPercentageSegmentedControl.buttons.element(boundBy: 1)
